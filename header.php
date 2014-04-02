e4f92d3865c681fd31ff6a68b833c65b209ba042
jjwdowik <jjwdowik@umich.edu>
2014-04-02 19:59:17 -0400

    header working

--- header.php
+++ header.php
@@ -4,13 +4,15 @@
 	</head>		
 		<nav class="clearfix" id="nav">
 			<ul class="clearfix">
-				<li id="current"><a href="#home">Home</a></li>
-				<li><a href="skills.php">Skills</a></li>
-				<li><a href="projects.php">Projects</a></li>
-				<li><a href="experiences.php">Experience</a></li>
-				<li><a href="ideas.php">Ideas</a></li>
+				<li class="index" id="current"><a href="index.php">Home</a></li>
+				<li class="skills"><a href="skills.php">Skills</a></li>
+				<li class="projects"><a href="projects.php">Projects</a></li>
+				<li class="experiences"><a href="experiences.php">Experience</a></li>
+				<li class="ideas"><a href="ideas.php">Ideas</a></li>
 			</ul>
 			<a href="#" id="pull">Menu</a>
 		</nav>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>		
+	<script src="js/main.js"></script>
 
 </html>
--- js/main.js
+++ js/main.js
@@ -16,15 +16,55 @@ $(function() {
 	});
 });
 
-$('nav ul li').click(function(){
-	$('nav ul li').removeAttr('id', 'current');
-	$(this).attr('id', 'current');;
+$('#nav ul li').click(function(){
+	$('#nav ul li').removeAttr('id', 'current');
+	$(this).attr('id', 'current');
 });
 
 //for scroll
 
 $(document).ready(function() {
-   $('#nav').localScroll({duration:800});
+  $('#nav').localScroll({duration:800});
+
+  var identifier = window.location.pathname;
+
+  var myString = identifier.substr(identifier.lastIndexOf("/") + 1);
+
+  switch(myString)
+        {
+            case'index.php':
+            $('#nav ul li').removeAttr('id', 'current');
+            $('.index').attr('id', 'current');
+            break;
+
+            case'skills.php':
+            //edit proper tag's css
+            $('#nav ul li').removeAttr('id', 'current');
+            $('.skills').attr('id', 'current');
+            break;
+
+            case'projects.php':
+            $('#nav ul li').removeAttr('id', 'current');
+            $('.projects').attr('id', 'current');
+            break;
+
+            case'experiences.php':
+            $('#nav ul li').removeAttr('id', 'current');
+            $('.experiences').attr('id', 'current');
+            break;
+
+            case'ideas.php':
+            $('#nav ul li').removeAttr('id', 'current');
+            $('.ideas').attr('id', 'current');
+            break;
+
+        }
+
+
+
 });
+
+
+
 
 
